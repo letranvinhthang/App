e6a827846a622cc236d8ba304a7ba1b73b5f73da
tuilathinh2017 <tuilathinh2017@gmail.com>
2020-09-29 13:59:24 +0700

    update

--- ThietKeBH/ThietKeBH/Form/frmDangNhap.Designer.cs
+++ ThietKeBH/ThietKeBH/Form/frmDangNhap.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ThietKeBH
 {
-    partial class Form1
+    partial class frmDangNhap
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmDangNhap";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
