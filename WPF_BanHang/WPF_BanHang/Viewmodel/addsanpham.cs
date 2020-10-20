2d5ff13e5f62303ca4ffed65db21793a4eca7593
danghao12 <vudanghao12@gmail.com>
2020-10-20 14:32:44 +0700

    addsp

---
+++ WPF_BanHang/WPF_BanHang/Viewmodel/addsanpham.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPF_BanHang.Models;
+namespace WPF_BanHang.Viewmodel
+{
+   public class addsanpham :BaseViewModel
+    {
+        public ObservableCollection<DanhmucSanpham> _lsplist;
+
+        public ObservableCollection<DanhmucSanpham> lsplist { get => _lsplist; set { _lsplist = value; OnPropertyChanged(); } }
+        private string _ten;
+
+        public string ten { get => _ten; set { _ten = value; OnPropertyChanged(); } }
+        private int _masp;
+
+        public int masp { get => _masp; set { _masp = value; OnPropertyChanged(); } }
+        private int _soluong;
+
+        public int soluong { get => _soluong; set { _soluong = value; OnPropertyChanged(); } }
+        public ICommand addcommand { get; set; }
+        public ICommand exitcommand { get; set; }
+        public addsanpham()
+        {
+            var db = new qlbhContext();
+            lsplist = new ObservableCollection<DanhmucSanpham>(db.DanhmucSanpham);
+        }
+    }
+}
