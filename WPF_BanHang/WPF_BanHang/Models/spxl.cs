1c17fc4f75899ac1ff60e79bc2dce41d4e6da495
danghao12 <vudanghao12@gmail.com>
2020-10-21 23:29:47 +0700

    h

---
+++ WPF_BanHang/WPF_BanHang/Models/spxl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_BanHang.Models
+{
+    public class spxl
+    {
+        public long IdSanpham { get; set; }
+        public string TenSanpham { get; set; }
+        public string Loaisp { get; set; }
+        public bool SanphamHot { get; set; }
+        public bool SanphamMoi { get; set; }
+        public byte[] HinhSanpham { get; set; }
+        public bool XoaSanPham { get; set; }
+
+    }
+}
