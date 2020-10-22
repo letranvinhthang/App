6ec131036d369043daa2fcb406628682cab0bd7c
ctrung2k <ctrung2k@gmail.com>
2020-10-22 13:39:25 +0700

    trung

--- WPF_BanHang/WPF_BanHang/Models/Orderxl.cs
+++ WPF_BanHang/WPF_BanHang/Models/Orderxl.cs
@@ -6,7 +6,7 @@ namespace WPF_BanHang.Models
 {
     class Orderxl
     {
-
+        public byte[] hinhsp { get; set; }
         public long barcode { get; set; }
         public string tensp { get; set; }
         public double dongia { get; set; }
