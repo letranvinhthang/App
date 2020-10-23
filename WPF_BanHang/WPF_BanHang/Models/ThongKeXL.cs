b09d0aa4b424956f04d30bc5335e48ab6b9ad15b
ctrung2k <ctrung2k@gmail.com>
2020-10-23 11:49:10 +0700

    trung

---
+++ WPF_BanHang/WPF_BanHang/Models/ThongKeXL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_BanHang.Models
+{
+    class ThongKeXL
+    {
+        public byte[] hinhsp { get; set; }
+        public string tensp { get; set; }
+        public double dongia { get; set; }
+        public int IDcuahang { get; set; }
+    }
+}
---
+++ WPF_BanHang/WPF_BanHang/Viewmodel/ThongKeViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPF_BanHang.Models;
+
+namespace WPF_BanHang.Viewmodel
+{
+    class ThongKeViewModel : BaseViewModel
+    {
+        public ThongKeXL _SelectedItem;
+        public ThongKeXL SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value; OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    
+                }
+            }
+        }
+        public enum ChucNangQL
+        {
+            DoanhThu, SanPham, LichSu
+        };
+        private int _ChucNangThongKe;
+        public int ChucNangThongKe { get => _ChucNangThongKe; set { _ChucNangThongKe = value; OnPropertyChanged(); } }
+        public ICommand BtnDoanhThuCommand { get; set; }
+        public ICommand BtnSanPhamCommand { get; set; }
+        public ICommand BtnLichSuCommand { get; set; }
+
+        public ThongKeViewModel()
+        {
+            #region Nút chức năng thống kê
+            BtnDoanhThuCommand = new RelayCommand<Grid>((p) => { return true; }, (p) =>
+            {
+                ChucNangThongKe = (int)ChucNangQL.DoanhThu;
+            });
+            BtnSanPhamCommand = new RelayCommand<Grid>((p) => { return true; }, (p) =>
+            {
+                ChucNangThongKe = (int)ChucNangQL.SanPham;
+            });
+            BtnLichSuCommand = new RelayCommand<Grid>((p) => { return true; }, (p) =>
+            {
+                ChucNangThongKe = (int)ChucNangQL.LichSu;
+            });
+            #endregion
+        }
+    }
+    
+}
