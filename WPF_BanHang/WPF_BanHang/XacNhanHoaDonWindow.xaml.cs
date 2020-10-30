85f99932abeae2a0354cd23064cec6c5972c97b3
tuilathinh2017 <tuilathinh2017@gmail.com>
2020-10-30 11:24:50 +0700

    them form

---
+++ WPF_BanHang/WPF_BanHang/XacNhanHoaDonWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_BanHang
+{
+    /// <summary>
+    /// Interaction logic for XacNhanHoaDonWindow.xaml
+    /// </summary>
+    public partial class XacNhanHoaDonWindow : Window
+    {
+        public XacNhanHoaDonWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
