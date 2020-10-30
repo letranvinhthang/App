96f7710681392abcc8467beb60548ce94561ce24
ctrung2k <ctrung2k@gmail.com>
2020-10-30 16:43:28 +0700

    a

--- WPF_BanHang/WPF_BanHang/Models/qlbhContext.cs
+++ WPF_BanHang/WPF_BanHang/Models/qlbhContext.cs
@@ -194,6 +194,7 @@ namespace WPF_BanHang.Models
                 entity.HasOne(d => d.IdCuahangNavigation)
                     .WithMany(p => p.HoaDon)
                     .HasForeignKey(d => d.IdCuahang)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("hoa_don_ibfk_1");
 
                 entity.HasOne(d => d.IdKhachhangNavigation)
@@ -209,6 +210,7 @@ namespace WPF_BanHang.Models
                 entity.HasOne(d => d.IdNhanvienNavigation)
                     .WithMany(p => p.HoaDon)
                     .HasForeignKey(d => d.IdNhanvien)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("hoa_don_ibfk_2");
             });
 
