ef5b00738b449f737b172bb06e06b158233e4c77
tuilathinh2017 <tuilathinh2017@gmail.com>
2020-11-03 13:41:23 +0700

    ád

--- WPF_BanHang/WPF_BanHang/Models/qlbhContext.cs
+++ WPF_BanHang/WPF_BanHang/Models/qlbhContext.cs
@@ -35,7 +35,7 @@ namespace WPF_BanHang.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=192.168.10.228;database=qlbh;user=root", x => x.ServerVersion("10.4.14-mariadb"));
+                optionsBuilder.UseMySql("server=192.168.10.240;database=qlbh;user=root", x => x.ServerVersion("10.4.14-mariadb"));
             }
         }
 
