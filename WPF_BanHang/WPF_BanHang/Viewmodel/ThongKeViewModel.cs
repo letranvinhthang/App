72c9ef75d33b348d1becd7e8c295d2170da1d220
Phat <huynhcongphat2510@gmail.com>
2020-11-03 17:00:34 +0700

    a

--- WPF_BanHang/WPF_BanHang/Viewmodel/ThongKeViewModel.cs
+++ WPF_BanHang/WPF_BanHang/Viewmodel/ThongKeViewModel.cs
@@ -89,12 +89,7 @@ namespace WPF_BanHang.Viewmodel
                      //   thongke.NgayTao = item.NgayTao;
                         thongke.TongDoanhThu = item.TongDoanhThu;
                         _doanhthu.Add(double.Parse(item.TongDoanhThu.ToString()));
-<<<<<<< HEAD
                         Ngay.Add(item.NgayTao);
-=======
-                        //_soluongdon.Add(item.SoLuongHoaDon);
-                    //    _Ngay.Add(double.Parse(item.NgayTao.ToString("dd")));
->>>>>>> origin/hao1
                         doanhthulist.Add(thongke);
                     }
                 }
